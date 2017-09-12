b05b8d7d58b46eb714b9370f947363548c831ad8
Alexey Ponomarev <mielofon@gmail.com>
2017-09-12 23:59:36 +0300

    start 1

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client', 'babel-polyfill', './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack
+      .optimize
+      .OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'react-hot-loader', {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env'],
+              plugins: ['transform-runtime']
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+}
